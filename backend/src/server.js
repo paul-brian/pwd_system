7e9e0abe15cc735251cefb0676b2ccc2eb6f2c99
Paul Brian B. Duterte <dutertepaulbrian43@gmail.com>
2026-03-13 20:52:38 +0800

    fix start script for render and server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -72,6 +72,9 @@ app.use("/api/access", accessRoutes);
 app.use("/api/users", userRoutes);
 
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
